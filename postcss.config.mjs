b9334ca58084f9d5995c34490d1f62468fe6a47a
OscarRodriguezUniguajira <oirodriguez@uniguajira.edu.co>
2025-08-10 16:56:25 -0500

    commit nuevo

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {}, // el plugin que Next estaba pidiendo
+    autoprefixer: {},           // necesario para prefijos CSS en producción
   },
 }
 
